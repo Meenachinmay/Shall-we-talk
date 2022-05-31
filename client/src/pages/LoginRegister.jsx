285207bb72d103a8825a49c1eff7de5eb52c88e1
Chinmay Anand <chinmayanand896@gmail.com>
2022-06-01 02:50:44 +0530

    activate the client-side input validation for login-register page, fix login-register UI on mobile side and add little user feedback reference for input field

--- client/src/pages/LoginRegister.jsx
+++ client/src/pages/LoginRegister.jsx
@@ -2,6 +2,7 @@ import React, { useEffect, useState } from 'react'
 import { useSelector, useDispatch } from 'react-redux'
 import { userLogin, userRegister } from '../actions/user'
 import { useNavigate } from 'react-router-dom'
+import { setNewAlert } from '../actions/alert'
 
 import * as yup from 'yup';
 
@@ -9,11 +10,11 @@ const LoginRegister = () => {
     
     // getting react hooks
     const dispatch = useDispatch()
-    const loadeduser = useSelector(state => state.user)
+    const loadUserFromReduxStore = useSelector(state => state.user)
     const navigate = useNavigate()
 
     // getting data from redux store
-    const { user, loading, error, login, logout } = loadeduser
+    const { user, loading, error, login, logout } = loadUserFromReduxStore
 
     // states for login
     const [keepMeLoggedIn, setKeepMeLoggedIn] = useState(false)
@@ -28,13 +29,13 @@ const LoginRegister = () => {
     // schemas for validation check
     let loginSchema = yup.object().shape({
         email: yup.string().email().required(),
-        password: yup.string().required()
+        password: yup.string().required().min(6).max(30)
     })
 
     let registerSchema = yup.object().shape({
         name: yup.string().required().max(36),
         email: yup.string().email().required(),
-        password: yup.string().required()
+        password: yup.string().required().min(6).max(30)
     })
 
     useEffect(() => {
@@ -43,20 +44,52 @@ const LoginRegister = () => {
         }
     })
 
+    // handle login request
     const handleLogin = () => {
-        dispatch(userLogin(email, password, navigate, keepMeLoggedIn))
+        loginSchema.isValid({
+            email: email,
+            password: password
+        })
+        .then(function (valid) {
+            if (valid) {
+                dispatch(userLogin(email, password, navigate, keepMeLoggedIn))
+            } else {
+                // create a new alert type and send it
+                const newalert = {
+                    type: 'danger',
+                    message: 'Please fill all the inputs correctly.'
+                }
+                dispatch(setNewAlert(newalert))
+            }
+        })
     }
 
+    // handle register request
     const handleRegister = () => {
-        dispatch(userRegister(name, emailRegister, passwordRegister))   
+        registerSchema.isValid({
+            name: name,
+            email: emailRegister,
+            password: passwordRegister
+        })
+        .then( function (valid) {
+            if (valid) {
+                dispatch(userRegister(name, emailRegister, passwordRegister))  
+            } else {
+                // create a new alert type and send it
+                const newalert = {
+                    type: 'danger',
+                    message: 'Please fill all the inputs correctly.'
+                }
+                dispatch(setNewAlert(newalert))
+            }
+        }) 
     }
 
     return (
         <div className='container mx-auto'>
-           <div className='flex flex-col sm:flex sm:flex-row items-center justify-center sm:space-x-2 space-x-0 p-10'>
-               
+           <div className='flex flex-col sm:flex sm:flex-row items-center justify-center sm:space-x-2 space-x-0 sm:p-10 p-4'>
                {/* Login screen */}
-                <div className='flex flex-grow flex-col mt-12 w-full sm:w-1/3 p-10 bg-white border-1 shadow-lg rounded-lg' style={{height: '400px'}}>
+                <div className='flex flex-grow flex-col mt-12 w-full sm:w-1/3 p-5 bg-white border-1 shadow-lg rounded-lg' style={{height: '400px'}}>
                     <div className='flex flex-col items-center justify-center pt-8 text-center'>
                         <h1 className='text-3xl text-indigo-600'>ログイン</h1>
                         <span className='text-xs text-gray-400'>既にアカウントをお持ちの方はこちら</span>
@@ -65,13 +98,21 @@ const LoginRegister = () => {
                         <div className='flex flex-col items-center justify-start space-x-2'>
                             <input onChange={(e) => setEmail(e.target.value)} className='border border-gray-400 rounded focus:outline-none px-2 py-1 w-full' type="text" placeholder="メールを入力ください" />
                         </div>
+                        <p className='text-[10px] text-red-600'>Name is required.</p>
                         <div className='flex flex-col items-center space-x-2'>
                             <input onChange={(e) => setPassword(e.target.value)} className='border border-gray-400 rounded focus:outline-none px-2 py-1 w-full' type="password" placeholder="パスワードを入力ください" />
                         </div>
+                        <p className='text-[10px] text-red-600'>Password is required.</p>
                     </div>
-                    <div onClick={handleLogin} className='bg-indigo-600 text-center p-1 mt-4 rounded text-white cursor-pointer hover:bg-indigo-400'>
-                        ログイン
-                    </div>
+                    {
+                        loading ? 
+                        <button disabled={true} className='bg-yellow-600 text-center p-1 mt-4 rounded text-white cursor-disabled hover:bg-indigo-400'>
+                            ログイン中
+                        </button> :  
+                        <button onClick={handleLogin} className='bg-indigo-600 text-center p-1 mt-4 rounded text-white cursor-pointer hover:bg-indigo-400'>
+                            ログイン
+                        </button>
+                    }
                     <div className='flex items-center mt-4 space-x-2'>
                         <input type='checkbox' className='' onChange={(e) => setKeepMeLoggedIn(e.target.checked)}/>
                         <span className='text-sm text-gray-400'>ログインしたままにする</span>
@@ -82,7 +123,7 @@ const LoginRegister = () => {
                 <span className='hidden sm:block text-indigo-600 text-2xl p-4'>又は</span>
                 
                 {/* Register screen */}
-                <div className='flex flex-grow flex-col mt-12 w-full sm:w-2/6 p-10 bg-white border-1 shadow-lg rounded-lg' style={{height: '400px'}}>
+                <div className='flex flex-grow flex-col mt-12 w-full sm:w-2/6 p-5 bg-white border-1 shadow-lg rounded-lg' style={{height: '400px'}}>
                     <div className='flex flex-col items-center justify-center pt-8 text-center'>
                     <h1 className='text-3xl text-indigo-600'>新規登録</h1>
                     <span className='text-xs text-gray-400'>新規登録の場合はこちら</span>
@@ -91,18 +132,20 @@ const LoginRegister = () => {
                         <div className='flex flex-col items-center justify-start space-x-2'>
                             <input onChange={(e) => setName(e.target.value)} className='border border-gray-400 rounded focus:outline-none px-2 py-1 w-full' type="text" placeholder="お名前を入力ください" required/>
                         </div>
+                        <p className='text-[10px] text-red-600'>Name is required.</p>
                         <div className='flex flex-col items-center space-x-2'>
                             <input onChange={(e) => setEmailRegister(e.target.value)} className='border border-gray-400 rounded focus:outline-none px-2 py-1 w-full' type="email" placeholder="メールを入力ください" required/>
                         </div>
+                        <p className='text-[10px] text-red-600'>Email should be a valid email and it is required.</p>
                         <div className='flex flex-col items-center justify-start space-x-2'>
                             <input onChange={(e) => setPasswordRegister(e.target.value)} className='border border-gray-400 rounded focus:outline-none px-2 py-1 w-full' type="password" placeholder="パスワードを入力ください" required/>
                         </div>
+                        <p className='text-[10px] text-red-600'>Password must contains at least 6 letters and it is required.</p>
                         <div onClick={handleRegister} className='bg-indigo-600 text-center p-1 mt-4 rounded text-white cursor-pointer hover:bg-indigo-400'>
                             登録する
                         </div>
                     </div>
                 </div>
-
            </div>
         </div>
     )
