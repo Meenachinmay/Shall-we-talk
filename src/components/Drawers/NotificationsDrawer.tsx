23ad453e649d28695d2a2e5cfc1a7562b4083598
Chinmay Anand <chinmayanand896@gmail.com>
2022-12-27 01:32:50 +0530

    no notification message set

--- src/components/Drawers/NotificationsDrawer.tsx
+++ src/components/Drawers/NotificationsDrawer.tsx
@@ -1,4 +1,4 @@
-import React, { useEffect, useRef, useState } from 'react'
+import React, { useEffect, useRef, useState } from "react";
 import {
   Drawer,
   DrawerBody,
@@ -9,64 +9,64 @@ import {
   useDisclosure,
   Tooltip,
   IconButton,
-} from '@chakra-ui/react'
-import { HiBell } from 'react-icons/hi'
-import NotificationCard from '../Notifications/NotificationCard'
-import { NotificationType } from '../../types/NotificationType'
-import { collection, onSnapshot, query, where } from 'firebase/firestore'
-import { firestore } from '../../firebase/clientApp'
-import { useRecoilState } from 'recoil'
-import { currentUserState } from '../../atoms/currentUserState'
-import LoadingSpinner from '../customUIComponents/LoadingSpinner'
+  Text,
+} from "@chakra-ui/react";
+import { HiBell } from "react-icons/hi";
+import NotificationCard from "../Notifications/NotificationCard";
+import { NotificationType } from "../../types/NotificationType";
+import { collection, onSnapshot, query, where } from "firebase/firestore";
+import { firestore } from "../../firebase/clientApp";
+import { useRecoilState } from "recoil";
+import { currentUserState } from "../../atoms/currentUserState";
+import LoadingSpinner from "../customUIComponents/LoadingSpinner";
 
 const NotificationsDrawer = () => {
-  const { isOpen, onOpen, onClose } = useDisclosure()
-  const [currentUser, setCurrentUserState] = useRecoilState(currentUserState)
-  const btnRef = useRef().current
-  const [notifications, setNotifications] = useState<NotificationType[]>([])
-  const notificationCol = collection(firestore, 'notifications')
-  const [loading, setLoading] = useState(false)
-  const [isNotificationNull, setIsNotificationNull] = useState<boolean>(false)
+  const { isOpen, onOpen, onClose } = useDisclosure();
+  const [currentUser, setCurrentUserState] = useRecoilState(currentUserState);
+  const btnRef = useRef().current;
+  const [notifications, setNotifications] = useState<NotificationType[]>([]);
+  const notificationCol = collection(firestore, "notifications");
+  const [loading, setLoading] = useState(false);
 
   useEffect(() => {
     try {
-      const fetchNotificationQuery = query(notificationCol, where("receiver", "==", `${currentUser.id}`))
-      setLoading(true)
-      onSnapshot(fetchNotificationQuery, snapShot => {
-        const notificationsLocal: NotificationType[] = []
+      const fetchNotificationQuery = query(
+        notificationCol,
+        where("receiver", "==", `${currentUser.id}`)
+      );
+      setLoading(true);
+      onSnapshot(fetchNotificationQuery, (snapShot) => {
+        const notificationsLocal: NotificationType[] = [];
         snapShot.forEach((doc) => {
-          if (!doc.data()) {
-            setIsNotificationNull(true)
-            return
-          }
-          notificationsLocal.push(doc.data() as NotificationType)
-        })
-        setNotifications(notificationsLocal)
-        setLoading(false)
-      })
+          notificationsLocal.push(doc.data() as NotificationType);
+        });
+        setNotifications(notificationsLocal);
+        setLoading(false);
+      });
     } catch (error) {
-      console.log("fetch notification service error " + error)
+      console.log("fetch notification service error " + error);
     }
-  }, [firestore])
+  }, [firestore, isOpen]);
 
   return (
     <>
       <Tooltip label="Notification" placement="bottom">
-        <IconButton ref={btnRef}
+        <IconButton
+          ref={btnRef}
           onClick={onOpen}
           color="red.500"
           icon={<HiBell />}
           mr={2}
           ml={2}
           size="sm"
-          display={{ base: 'none', sm: 'inherit' }}
+          display={{ base: "none", sm: "inherit" }}
           aria-label="Notification"
         />
       </Tooltip>
 
       <Drawer
         isOpen={isOpen}
-        placement='right'
+        placement="right"
         onClose={onClose}
         finalFocusRef={btnRef}
         size="md"
@@ -76,22 +76,29 @@ const NotificationsDrawer = () => {
           <DrawerCloseButton />
           <DrawerHeader>Notifications</DrawerHeader>
 
-          {
-            loading ? <DrawerBody><LoadingSpinner /></DrawerBody> :
-              <DrawerBody>
-                {
-                  notifications.map((notification) => (
-                    <NotificationCard key={notification.message} seen={notification.seen} type={notification.type} message={notification.message}/>
-                  ))
-                }
-                {isNotificationNull ? <p>No notifications.</p> : null}
-              </DrawerBody>
-          }
-
+          {loading ? (
+            <DrawerBody>
+              <LoadingSpinner />
+            </DrawerBody>
+          ) : (
+            <DrawerBody>
+              {notifications.map((notification) => (
+                <NotificationCard
+                  key={notification.message}
+                  seen={notification.seen}
+                  type={notification.type}
+                  message={notification.message}
+                />
+              ))}
+              {notifications.length === 0 ? (
+                <Text fontSize="sm">No notifications for you as of now.</Text>
+              ) : null}
+            </DrawerBody>
+          )}
         </DrawerContent>
       </Drawer>
     </>
-  )
-}
+  );
+};
 
-export default NotificationsDrawer
+export default NotificationsDrawer;
